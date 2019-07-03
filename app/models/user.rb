bd2ffda3456216a1736fd4d5a78f5ce4db4d9219
pnowik <pawel.nowik.97@gmail.com>
2019-07-03 15:38:58 +0200

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  avatar.attach(params[:avatar])
 end
