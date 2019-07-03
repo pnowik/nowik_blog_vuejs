2762723d42d6e16be209ca7142856e1e8b0b0a4e
pnowik <pawel.nowik.97@gmail.com>
2019-07-03 10:25:34 +0200

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'http://honest-poppy-39582.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://peaceful-lowlands-33312.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
