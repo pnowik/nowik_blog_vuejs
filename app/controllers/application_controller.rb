b790e0bdd4db2e0141245eb5b1aed41bfc86c0ee
pnowik <pawel.nowik.97@gmail.com>
2019-07-05 20:35:02 +0200

    Add pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
+include Pundit
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    redirect_to root_path
+    flash[:notice] = "You can't do this"
+  end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
